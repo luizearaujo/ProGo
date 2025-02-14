8d559b404f31d033e17a61e6417f0b59e8f424f9
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-02-14 09:34:23 -0300

    Implementation of formHandle and fix WillAttend tag name on html template

--- Cap01/PartyInvites/main.go
+++ Cap01/PartyInvites/main.go
@@ -46,6 +46,23 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
 		templates["form"].Execute(writer, formData{
 			Rsvp: &Rsvp{}, Errors: []string{},
 		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willAttend"][0] == "true",
+		}
+
+		responses = append(responses, &responseData)
+
+		if responseData.WillAttend {
+			templates["thanks"].Execute(writer, responseData.Name)
+		} else {
+			templates["sorry"].Execute(writer, responseData.Name)
+		}
 	}
 }
 
